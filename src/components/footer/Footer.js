d614079a5004baff2d7e630c4459ca81baf536b7
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-16 15:06:37 +0100

    ajout fichier footer.js

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import './footer.scss'
+import logo from '../../assets/logo_black.jpg'
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <img src={logo} alt="kasa"/>
+            <p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
+        </footer>
+    )
+}
