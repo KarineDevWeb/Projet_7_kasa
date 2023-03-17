18b3948b3d8df77592bda828f95228abc28b0b69
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-17 17:24:42 +0100

    ajout fichier ErrorPage.js

---
+++ src/pages/errorPage/ErrorPage.js
@@ -0,0 +1,18 @@
+import './errorPage.scss'
+import Header from "../../components/header/Header";
+import { Link } from 'react-router-dom';
+
+function ErrorPage() {
+    return (
+        <div className='ErrorPage'>
+            <Header />
+            <div className="errorPage_infos">
+                <h1 className='errorPage_infos_title'>404</h1>
+                <p className='errorPage_infos_content'>Oups! La page que vous demandez n'existe pas.</p>
+            </div>
+                <Link className='errorPage_infos_return' to='/kasa/'>Retourner sur la page d'accueil</Link>     
+        </div>
+    )
+}
+
+export default ErrorPage;
