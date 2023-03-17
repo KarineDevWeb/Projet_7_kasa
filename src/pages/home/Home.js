47c719ca8496d499fd30ae9bf95997220044a1cf
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-17 17:44:38 +0100

    ajout fichier Home.js

---
+++ src/pages/home/Home.js
@@ -0,0 +1,17 @@
+import './home.scss'
+import Header from '../../components/header/Header'
+import Banner from "../../components/banner/Banner"
+import Pictures from '../../components/pictures/Pictures'
+import Footer from '../../components/footer/Footer'
+
+export default function Home() {
+    return (
+        <div className='Home'>
+            <Header />
+            <Banner />
+            <Pictures/>
+            <Footer/>
+            
+        </div>
+    )
+}
