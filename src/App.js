7be79be2460caa4d3fb3912bec91f89a5ce4f608
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-17 13:05:22 +0100

    modifications ErrorPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
-import NotFound from "./pages/notFound/NotFound";
+import NotFound from "./pages/errorPage/ErrorPage";
 import Accommodation from "./pages/accommodation/Accommodation";
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
