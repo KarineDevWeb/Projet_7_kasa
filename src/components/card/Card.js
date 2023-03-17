31da14d843e9b5f64a01e9c46914427ad15ca638
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-17 13:49:12 +0100

    ajout fichier card.js

---
+++ src/components/card/Card.js
@@ -0,0 +1,13 @@
+import './Card.scss'
+import { Link } from 'react-router-dom'
+
+function Card({id, title, cover}) {
+    return (
+        <Link to={`/accommodation/${id}`}className="pictures_card">
+            <img src={cover} alt={title}/>
+            <h3>{title}</h3>
+        </Link>
+    )
+}
+
+export default Card;
