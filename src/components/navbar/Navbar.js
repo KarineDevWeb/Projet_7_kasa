9b9f3cfb0f902a4c7f26f48dc3268712b914fcec
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-29 21:06:43 +0200

    modification route

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -9,8 +9,8 @@ export default function Navbar() {
     return (
         <nav className='nav'>
             <ul className='nav_list'>
-                <li className={currentRoute === '/kasa/' ? 'nav_list_item_active' : 'nav_list_item'}>
-                    <Link to='/kasa/'>Accueil</Link>
+                <li className={currentRoute === '/' ? 'nav_list_item_active' : 'nav_list_item'}>
+                    <Link to='/'>Accueil</Link>
                 </li>
                 <li className={currentRoute === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
                     <Link to='/about'>A propos</Link>
