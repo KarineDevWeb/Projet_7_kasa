86c38ca3537d32d0f010878c3128440d8b9c9850
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-29 15:13:07 +0200

    ajout commentaires

--- src/components/carrousel/Carrousel.js
+++ src/components/carrousel/Carrousel.js
@@ -2,11 +2,11 @@ import './carrousel.scss'
 import ChevronRight from '../../assets/chevron_carrousel_right.png'
 import ChevronLeft from '../../assets/chevron_carrousel_left.png'
 import { useState } from 'react'
-
+// déclaration d'une variable dans le composant :
 function Slide({imageSlide}) {
-
+// j'obtiens l'état actuel (currentIndex) et la fonction qui me permet de la mettre à jour (setCurrentIndex)
     const [currentIndex,setCurrentIndex]=useState(0)
-// quand on arrive au dernier, on repart au premier
+// quand on arrive au dernier, on repart au premier et inversement
     const nextSlide=()=>{
         setCurrentIndex(currentIndex+1)
         if(currentIndex===imageSlide.length-1) 
@@ -18,7 +18,7 @@ function Slide({imageSlide}) {
         if(currentIndex===0)
         setCurrentIndex(imageSlide.length-1)
     }
-
+// au clic, sur les chevrons droit ou gauche, changement de photo
     return(
         <section style={{backgroundImage:`url(${imageSlide[currentIndex]})`}}className='carrousel'>
             {imageSlide.length>1&&
