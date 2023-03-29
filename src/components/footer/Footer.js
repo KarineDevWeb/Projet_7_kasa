810947b874c820f26a79c0d386b6ebb90300c741
karinedevweb <karineisabellesavoir@gmail.com>
2023-03-29 15:23:53 +0200

    dernières modifications

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,20 @@
-import './footer.scss'
+import'./footer.scss'
 import logo from '../../assets/logo_black.jpg'
 
 export default function Footer() {
     return (
         <footer className="footer">
-            <img src={logo} alt="kasa"/>
-            <p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
-        </footer>
-    )
-}
+            <img src={logo} alt="kasa" className='footer_img'/>
+        <p className='footer_copyright'>© 2020 Kasa. All rights reserved</p>
+    </footer>
+     )
+ }
+/* <footer className={footerClass.footerSticky}>
+    <div className={footerClass.footerFlex}>
+        <img draggale="false" src={logo} alt="Logo Kasa" className={footerClass.footerFlex_img}/>
+        <p className={footerClass.footerFlex_paragraph}>
+        © 2020 Kasa. All rights reserved 
+        </p>
+    </div>
+</footer>
+    ); */
